bba5918bae65e0957fb1cb9aa9aeec29b1b73037
Michael Mulvaney <mmulvaney@bna.com>
2014-09-26 13:18:57 -0400

    added packages

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,11 +2,23 @@
 # just install some packages
 
 package { [
+  'autoconf',
+  'bison',
+  'emacs',
+  'gcc-c++',
+  'git',
+  'libffi-devel',
+  'libtool',
+  'libxml2-devel',
+  'libxslt-devel',
+  'libyaml-devel',
+  'patch',
+  'readline-devel',
+  'openssl-devel',
+  'sqlite-devel',
   'ruby',
   'ruby-devel',
   'tmux',
-  'emacs',
-  'git',
  ]:
 
   ensure => present,
